7fdfb73dad55a6cc84c73eece2d09068f413b81a
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-16 09:59:54 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FunWithExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n****\nI'm done!!!\n****\n");
+        }
+        public static int divideInts(int a, int b)
+        {
+            return a / b;
+        }
+        public static int getSafeInt()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+        public static int sumOfNumbers(int[] numbers, int length)
+        {
+            int total = 0;
+            foreach (var num in numbers)
+            {
+                total += num;
+            }
+            return total;
+        }
+    }
+}
